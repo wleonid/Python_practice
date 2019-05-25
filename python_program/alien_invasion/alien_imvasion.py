03b5b7a4a1af047b529b6361b22086db5a1df5dd
wleonid <515125369@qq.com>
2019-05-25 22:51:21 +0800

    0.0.2

--- python_program/alien_invasion/alien_imvasion.py
+++ python_program/alien_invasion/alien_imvasion.py
@@ -1,17 +1,17 @@
-import sys
 import pygame
+from settings import Settings
+from ship import Ship
+import game_functions as gf
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200,800))
+    ai_settings=Settings()
+    screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-
+    ai_ship=Ship(screen)
+    bg_color=ai_settings.bg_color
     while True:
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-        
-        pygame.display.flip()
+        gf.check_events()
+        gf.update_screen(ai_settings,screen,ai_ship)
 
 run_game()
